b7951c601b1a66df57417442d979c3785e55f89d
mio-site <mio.in.wland@gmail.com>
2025-08-09 00:21:38 +0900

    いい感じに by gpt-5

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'ks-static-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/index.css',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  if (request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      const network = fetch(request).then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, resClone));
+        return res;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
